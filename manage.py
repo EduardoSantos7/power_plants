d10984dbc0d34cb94808898e121858b47ecf09f0
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-10-19 07:20:19 -0500

    migrations

---
+++ manage.py
@@ -0,0 +1,19 @@
+import os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from app import db, create_app
+from app import models
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+app = create_app(config_name=os.getenv('APP_SETTINGS'))
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
