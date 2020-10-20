4c12f26d57873046bfc51d94726259b7971aa41f
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-10-20 13:44:30 -0500

    more tests

--- test_power_plant.py
+++ test_power_plant.py
@@ -14,8 +14,8 @@ class PowerPlantTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
-        self.power_plant_query = {'n': 2, 'state_abbreviation': 'AK'}
         self.number_power_plants = [i for i in range(1, 100)]
+        self.test_state = "CA"
 
         # binds the app to the current context
         with self.app.app_context():
@@ -23,19 +23,48 @@ class PowerPlantTestCase(unittest.TestCase):
             db.create_all()
 
     def test_api_can_get_n_power_plants(self):
-        """Test API can get a power plants (GET request)."""
+        """Test API can get N power plants (GET request)."""
         for n in self.number_power_plants:
             response = self.client().get(f'/power_plants/', query_string={'number_plants': n})
             power_plants = json.loads(response.data)
             self.assertEqual(response.status_code, 200)
             self.assertEqual(len(power_plants), n)
 
-    def tearDown(self):
-        """teardown all initialized variables."""
-        with self.app.app_context():
-            # drop all tables
-            db.session.remove()
-            db.drop_all()
+    def test_api_can_get_n_power_plants_from_state(self):
+        """Test API can get N power plants of a given state (GET request)."""
+        for n in self.number_power_plants:
+            response = self.client().get(
+                f'/power_plants/', query_string={
+                    'number_plants': n,
+                    'state_abbreviation': self.test_state
+                })
+            self.assertEqual(response.status_code, 200)
+            power_plants = json.loads(response.data)
+            self.assertEqual(len(power_plants), n)
+
+            for power_plant in power_plants:
+                self.assertEqual(self.test_state, power_plant.get('state_abbreviation'))
+
+    def test_api_can_get_all_power_plants_from_state(self):
+        """Test API can get all the power plants of a given state (GET request)."""
+        for n in self.number_power_plants:
+            response = self.client().get(
+                f'/power_plants/', query_string={
+                    'state_abbreviation': self.test_state
+                })
+            power_plants = json.loads(response.data)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(1613, len(power_plants))
+
+            for power_plant in power_plants:
+                self.assertEqual(self.test_state, power_plant.get('state_abbreviation'))
+
+    # def tearDown(self):
+    #     """teardown all initialized variables."""
+    #     with self.app.app_context():
+    #         # drop all tables
+    #         db.session.remove()
+    #         db.drop_all()
 
 
 # Make the tests conveniently executable
