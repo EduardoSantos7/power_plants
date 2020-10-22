2561d97ee59731fcf81fe23bc5bf96ae4d6a63e6
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-10-22 10:03:19 -0500

    change in configs

--- instance/config.py
+++ instance/config.py
@@ -20,7 +20,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://default:default@localhost:5432/test_power_plants_db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL')
     DEBUG = True
 
 
